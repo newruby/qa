35bf13e5300fd6ad7d9313dd878fa408e6c8b22a
newruby <757336630@qq.com>
2019-05-30 15:40:46 +0800

    deploy

--- src/main/java/com/ty/qa/QaApplication.java
+++ src/main/java/com/ty/qa/QaApplication.java
@@ -2,9 +2,10 @@ package com.ty.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QaApplication {
+public class QaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(QaApplication.class, args);
