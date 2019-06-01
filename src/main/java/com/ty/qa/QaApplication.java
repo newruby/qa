d122ed40c71bd4522a040c3fa35cc4787941e2bb
newruby <757336630@qq.com>
2019-06-01 17:15:37 +0800

    builder

--- src/main/java/com/ty/qa/QaApplication.java
+++ src/main/java/com/ty/qa/QaApplication.java
@@ -2,10 +2,15 @@ package com.ty.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class QaApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(QaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QaApplication.class, args);
